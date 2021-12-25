d9e1f9901ac4ffdb57f66e2865ba5547bbd26230
Anh Duong <duonanh195@gmail.com>
2021-12-26 02:46:04 +0700

    added font-awesome icon

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,7 +11,7 @@ class SessionsController < ApplicationController
       log_in user
       redirect_to user
     else
-      flash[:danger] = 'Invalid email/password combination'
+      flash[:danger] = 'Sai thông tin đăng nhập. Vui lòng nhập lại.'
       # Create an error message.
       render 'new'
     end
